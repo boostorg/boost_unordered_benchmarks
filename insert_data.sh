e63b00a6d07fb9df40ce016045bad47628363e12
Peter Dimov <pdimov@gmail.com>
2022-12-09 04:46:34 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python2 insert_data.py gcc-x86/string.cpp.txt      README.md
-python2 insert_data.py gcc-x86/string_view.cpp.txt README.md
-python2 insert_data.py gcc-x86/uint32.cpp.txt      README.md
-python2 insert_data.py gcc-x86/uint64.cpp.txt      README.md
-python2 insert_data.py gcc-x86/uuid.cpp.txt        README.md
-python2 insert_data.py gcc-x86/word_count.cpp.txt  README.md
-python2 insert_data.py gcc-x86/word_size.cpp.txt   README.md
+python2 insert_data.py gcc-x64/string.cpp.txt      README.md
+python2 insert_data.py gcc-x64/string_view.cpp.txt README.md
+python2 insert_data.py gcc-x64/uint32.cpp.txt      README.md
+python2 insert_data.py gcc-x64/uint64.cpp.txt      README.md
+python2 insert_data.py gcc-x64/uuid.cpp.txt        README.md
+python2 insert_data.py gcc-x64/word_count.cpp.txt  README.md
+python2 insert_data.py gcc-x64/word_size.cpp.txt   README.md
 
 python2 insert_data.py clang-x64/string.cpp.txt      README.md
 python2 insert_data.py clang-x64/string_view.cpp.txt README.md
