3f462db29fa3b05e3c473e6dba80d69ef6b8ea6f
joaquintides <joaquin@tid.es>
2023-01-16 12:45:20 +0100

    added clang-x86, synced insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,8 +8,8 @@ python2 insert_data.py clang-x64/main.cpp.txt README.md
 
 python2 insert_data.py clang-arm64/main.cpp.txt README.md
 
-#python2 insert_data.py gcc-x86/main.cpp.txt README.md
+python2 insert_data.py gcc-x86/main.cpp.txt README.md
 
-#python2 insert_data.py clang-x86/main.cpp.txt README.md
+python2 insert_data.py clang-x86/main.cpp.txt README.md
 
 #python2 insert_data.py vs-x86/main.cpp.txt README.md
