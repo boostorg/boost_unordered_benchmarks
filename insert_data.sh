b3f04c0e8e5c7c31ee53b2f30f57706566cb9e14
joaquintides <joaquin@tid.es>
2023-01-20 20:16:10 +0100

    extended insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -4,7 +4,7 @@ python2 insert_data.py gcc-x64/main.cpp.txt README.md
 
 python2 insert_data.py clang-x64/main.cpp.txt README.md
 
-#python2 insert_data.py vs-x64/main.cpp.txt README.md
+python2 insert_data.py vs-x64/main.cpp.txt README.md
 
 python2 insert_data.py clang-arm64/main.cpp.txt README.md
 
@@ -12,4 +12,4 @@ python2 insert_data.py gcc-x86/main.cpp.txt README.md
 
 python2 insert_data.py clang-x86/main.cpp.txt README.md
 
-#python2 insert_data.py vs-x86/main.cpp.txt README.md
+python2 insert_data.py vs-x86/main.cpp.txt README.md
